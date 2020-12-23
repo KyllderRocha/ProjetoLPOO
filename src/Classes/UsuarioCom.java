89ec3770b43171023d64885380d973dfdd05e54b
Kyllder Rocha <kyllder.rocha@gmail.com>
2020-12-23 11:25:51 -0300

    Adicionando classe abstrata

--- src/Classes/UsuarioCom.java
+++ src/Classes/UsuarioCom.java
@@ -1,10 +1,5 @@
 package Classes;
 
-
-
-
-import Classes.UsuarioNormal;
-
 import Modelo.Usuarios;
 
 /**
@@ -17,26 +12,8 @@ import Modelo.Usuarios;
 
  */
 
-public class UsuarioCom  implements Usuarios{
+public class UsuarioCom extends UsuarioAbstrato{
 
-    /**Uma String onde vai ser armazenado o nome do Usario*/
-    private String nome;
-    /**Uma String onde vai ser armazenado o cpf do Usúario */
-     private String cpf;
-    /**Uma String onde vai ser armazenado o rg do Usúario */
-    private String rg;
-    /**Uma String onde vai ser armazenado o telefone do Usúario */
-    private String telefone;
-    /**Uma String onde vai ser armazenado o endereço do Usúario */
-    private String endereco= null;
-    /**Uma String onde vai ser armazenado o tipo de Usúario */
-    private final String tipo="Comerciario";
-    /**Um inteiro onde vai ser armazenado a quantidade de Obras que o Usúario pode pegar */
-    private final int qtdObras=3;
-    /**Uma double onde vai ser armazenado a divida do Usúario */
-    private double divida=0;
-    
-    
 
      /**
 
@@ -49,19 +26,9 @@ public class UsuarioCom  implements Usuarios{
       */
 
     public UsuarioCom(String nome,String cpf,String rg,String telefone,String endereco){
-
-        this.nome=nome;
-
-        this.cpf=cpf;
-
-        this.rg=rg;
-
-        this.telefone=telefone;
-
-        this.endereco=endereco;
-
-        
-
+    	
+    	super(nome,cpf,rg,telefone,endereco,"Comerciario", 3);
+       
     }
 
     /**
@@ -77,291 +44,42 @@ public class UsuarioCom  implements Usuarios{
 
     public UsuarioCom(String nome,String cpf,String rg,String telefone,String endereco, double divida){
 
-        this.nome=nome;
-
-        this.cpf=cpf;
-
-        this.rg=rg;
-
-        this.telefone=telefone;
-
-        this.endereco=endereco;
-
-        this.divida=divida;
+    	super(nome,cpf,rg,telefone,endereco,divida,"Comerciario", 3);
 
     }
-
-    /**
-
-     * Metodo para retorno do nome do Ususario Comercial
-
-     * @return nome do Usuario
-
-     */
-
-    @Override
-
-    public String getNome() {
-
-        return this.nome;
-
-    }
-
-    /**
-    * Metodo para modificar o nome do Usuario Comercial
-    * @param nome novo nome
-    */
-
-    @Override
-
-    public void setNome(String nome) {
-
-        this.nome=nome;    
-
-    }
-
-    /**
-
-     * Metodo para retorno do cpf do Ususario Comercial
-
-     * @return cpf do Ususario
-
-     */
-
-    @Override
-
-    public String getCPF() {
-
-        return this.cpf;
-
-    }
-
+    
     /** 
 
-    * Metodo para modificar o cpf do Usuario Comercial
-   * @param cpf novo cpf
+     * Metodo para modificar o tipo do Usuario, podendo ir para Usuario funcionario ou Usuario normal
 
-    */
+     * @return Usuarios o novo usuario formado
 
-    @Override
-
-    public void setCPF(String cpf) {
-
-        this.cpf=cpf;
-
-    }
-
-    /**
-
-     * Metodo para retorno do rg do Ususario Comercial
-
-     * @return rg do Ususario
+     * @param tipo o novo tipo
 
      */
 
-    @Override
+     @Override
 
-    public String getRG() {
+     public Usuarios setTipo(String tipo) {
 
-        return this.rg;
+         if (tipo.equalsIgnoreCase("funcionario")) {
 
-    }
+            Usuarios b=new UsuarioFunc(this.getNome(), this.getCPF(), this.getRG(), this.getTelefone(), this.getEndereco(),this.getDivida());
 
-    /** 
+            return b;
 
-    * Metodo para modificar o rg do Usuario Comercial
-     * @param rg novo rg
+         }else if (tipo.equalsIgnoreCase("normal")) {
 
-    */
+              Usuarios b=new UsuarioNormal(this.getNome(), this.getCPF(), this.getRG(), this.getTelefone(), this.getEndereco(),this.getDivida());
 
-    @Override
+            return b;
 
-    public void setRG(String rg) {
+         }
 
-        this.rg=rg;
+         return null;
 
-    }
+     }
 
-    /**
-
-     * Metodo para retorno do tipo do Ususario 
-
-     * @return tipo do Ususario 
-
-     */
-
-    @Override
-
-    public String getTipo() {
-
-        return this.tipo;
-
-    }
-
-    /** 
-
-    * Metodo para modificar o tipo do Usuario, podendo ir para Usuario funcionario ou Usuario normal
-
-    * @return Usuarios o novo usuario formado
-
-    * @param tipo o novo tipo
-
-    */
-
-    @Override
-
-    public Usuarios setTipo(String tipo) {
-
-        if (tipo.equalsIgnoreCase("funcionario")) {
-
-           Usuarios b=new UsuarioFunc(nome, cpf, rg, telefone, endereco,divida);
-
-           return b;
-
-        }else if (tipo.equalsIgnoreCase("normal")) {
-
-             Usuarios b=new UsuarioNormal(nome, cpf, rg, telefone, endereco,divida);
-
-           return b;
-
-        }
-
-        return null;
-
-    }
-
-    /**
-
-     * Metodo para retorno do telefone do Ususario Comercial
-
-     * @return telefone do Ususario
-
-     */
-
-    @Override
-
-    public String getTelefone() {
-
-        return this.telefone;
-
-    }
-
-    /** 
-
-    * Metodo para modificar o telefone do Usuario Comercial
-     * @param telefone o novo telefone
-
-    */
-
-    @Override
-
-    public void setTelefone(String telefone) {
-
-        this.telefone=telefone;
-
-    }
-
-    /**
-
-     * Metodo para retorno do endereco do Ususario Comercial
-
-     * @return endereco do Ususario
-
-     */
-
-    @Override
-
-    public String getEndereco() {
-
-        return this.endereco;
-
-    }
-
-    /** 
-
-    * Metodo para modificar o endereco do Usuario Comercial
-   * @param Endereco novo endereço
-
-    */
-
-    @Override
-
-    public void setEndereco(String Endereco) {
-
-        this.endereco=Endereco;
-
-    }
-
-    /**
-
-     * Metodo para retorno da quantidade de obras do Ususario Comercial
-
-     * @return a quantidades de Obras que o Usuario pode pegar
-
-     */
-
-    @Override
-
-    public int getQtdObras() {
-
-        return qtdObras;
-
-    }
-
-    /**
-
-     * Metodo para retorno se o Ususario está devendo ou não
-
-     * @return se o Usuario está devendo
-
-     */
-
-    @Override
-
-    public boolean isDevendo() {
-
-        if (divida>0) {
-
-            return true;
-
-        }
-
-        return false;
-
-    }
-
-    /**
-
-     * Metodo para retorno das dividas do Ususario Comercial
-
-     * @return a divida do Ususario
-
-     */
-
-    @Override
-
-    public double getDivida() {
-
-        
-
-        return this.divida;
-
-    }
-
-    /** 
-
-    * Metodo para modificar as dividas do Usuario Comercial
-    * @param divida a nova divida
-
-    */
-
-    @Override
-
-    public void setDivida(double divida) {
-
-            this.divida=divida;    
-
-    }
 
     /**
 
